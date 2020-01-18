dac547c4c4cf9e747fc62381827a9d44414f1d14
SHARON ZHENG <sharzheng@gmail.com>
2020-01-18 16:58:21 -0500

    Updates

---
+++ precache-manifest.c7a17f841b51ee7bed958c010fca76df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96672baa9240d6deb9cba9a5e4bfb637",
+    "url": "/chinese-zodiac/index.html"
+  },
+  {
+    "revision": "55ff55f74759a9965d1d",
+    "url": "/chinese-zodiac/static/css/main.d4c23063.chunk.css"
+  },
+  {
+    "revision": "c3efff812bc4de7be08a",
+    "url": "/chinese-zodiac/static/js/2.ca6498b8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chinese-zodiac/static/js/2.ca6498b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55ff55f74759a9965d1d",
+    "url": "/chinese-zodiac/static/js/main.e2e8fc5f.chunk.js"
+  },
+  {
+    "revision": "a46749df515e4c3aa41d",
+    "url": "/chinese-zodiac/static/js/runtime-main.15314daa.js"
+  }
+]);
