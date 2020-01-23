4e77d805b384ab7dc5e96707ef40af64febd6517
SHARON ZHENG <sharzheng@gmail.com>
2020-01-23 12:56:00 -0500

    style changes

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,6 @@ import { useHistory } from "react-router-dom";
 import './Main.css';
 
 function Main(props) {
-  // const {year, setYear} = props;
   const history = useHistory();
 
   function nextPage() {
@@ -13,8 +12,8 @@ function Main(props) {
   //星座?
   return (
     <main className="main">
-      <h1>你是哪个生肖?</h1>
-      <h3>Which Chinese Zodiac Sign Are You?</h3>
+      <h1>你属什么的?</h1>
+      <h3>Which Chinese Zodiac Animal Are You?</h3>
       <button onClick={nextPage}>&rarr;</button>
     </main>
   );
